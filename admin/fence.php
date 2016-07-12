85ab8c1c7c009c558f2cf9b2a7a543ca9509fe4f
tompadam17 <tompadam@gmail.com>
2016-07-12 20:23:01 +0530

    fence.php changes

--- admin/fence.php
+++ admin/fence.php
@@ -1,147 +1,91 @@
+<?php
+   require_once("../secure/db.php");
+   session_start();
+  if(!isset($_SESSION['admin'])){
+      //header("location:login.php");
+      die("Something went wrong");
+   }
 
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Encoding methods</title>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <?php include('header.php'); ?>
-    <meta charset="utf-8">
-    <style>
-      #right-panel {
-        font-family: 'Roboto','sans-serif';
-        line-height: 30px;
-        padding-left: 10px;
+   else
+   {
+
+      $uname = $_SESSION['admin'];
+      $id = $_GET['id'];
+      $sql = "SELECT * FROM student WHERE org_id is NULL ";
+      $result = mysqli_query($db,$sql);      
+      $sql2 = "SELECT * FROM org ";
+
+      $result2 = mysqli_query($db,$sql2);
+
+       while($data = $result->fetch_assoc()) {
+          $student_data[] = $data;
       }
 
-      #right-panel select, #right-panel input {
-        font-size: 15px;
+       while($data = $result2->fetch_assoc()) {
+          $org_data[] = $data;
       }
 
-      #right-panel select {
-        width: 100%;
-      }
 
-      #right-panel i {
-        font-size: 12px;
-      }
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-      #map {
-        height: 80%;
-        width: 50%;
-        float: left;
-      }
-      #right-panel {
-        width: 46%;
-        float: left;
-      }
-      #encoded-polyline {
-        height: 100px;
-        width: 100%;
-      }
-    </style>
-  </head>
-  <body>
-  <header>
- <nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
       
+
+      #echo '<pre>';
+      #print_r($row);
+      #echo $uname;
+      #echo '</pre>';
+      #die();      
+      $count = mysqli_num_rows($result);
+
+   }
+
+?>
+
+<?php include('../header.php'); ?>
+<link rel="stylesheet" type="text/css" href="../assets/css/profile.css">
+
+<title>Profile</title>
+
+</head>
+
+<body>
+<header>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">
+        <img alt="Geolocation" class="logo" src="../assets/images/logo.png">
+      </a>
     </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="about.html" target="">About <span class="sr-only">(current)</span></a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> Contact Us <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Arun V.B</a></li>
-            <li><a href="#">Dennis K Bijo</a></li>
-            <li><a href="#">Johan varghese</a></li>
-            <li><a href="#">Sebin Paul</a></li>
-            <li role="separator" class="divider"></li>
-
-          </ul>
-        </li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Services <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Active Users</a></li>
-            <li><a href="#">Recent Users</a></li>
-            <li><a href="#">User locations</a></li>
-            <li role="separator" class="divider"></li>
-
-          </ul>
-        </li>
-      </ul>
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" id="place-input" class="form-control" placeholder="Search">
-        </div>
-        <button class="btn btn-default">Submit</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-       
-           <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
-      <ul id="login-dp" class="dropdown-menu">
-        <li>
-           <div class="row">
-              <div class="col-md-12">
-                Login via
-                <div class="social-buttons">
-                  <a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
-                  <a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
-                </div>
-                                or
-                 <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
-                    <div class="form-group">
-                       <label class="sr-only" for="exampleInputEmail2">Email address</label>
-                       <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
-                    </div>
-                    <div class="form-group">
-                       <label class="sr-only" for="exampleInputPassword2">Password</label>
-                       <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
-                                             <div class="help-block text-right"><a href="">Forget the password ?</a></div>
-                    </div>
-                    <div class="form-group">
-                       <button type="submit" class="btn btn-primary btn-block">Sign in</button>
-                    </div>
-                    <div class="checkbox">
-                       <label>
-                       <input type="checkbox"> keep me logged-in
-                       </label>
-                    </div>
-                 </form>
-              </div>
-              <div class="bottom text-center">
-                New here ? <a href="#"><b>Join Us</b></a>
-              </div>
-           </div>
-        </li>
-      </ul>
-        </li>
-        </li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
+  </div>
 </nav>
 </header>
+
+
+
+
+
+<div class="container-fluid">
+    <div class="row profile">
+		<?php include 'sidebar.php'; ?>
+		<div class="col-md-9">
+            <div class="profile-content">
+
+            <legend>Location Data</legend>
+
+            <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" id="place-input" class="form-control input-lg" placeholder="Search">
+        </div>
+        
+      </form>
+
+            		
     <div id="map"></div>
     <div id="right-panel">
       <div>Encoding:</div>
-      <textarea id="encoded-polyline"></textarea>
+      <textarea style="    width: 100%;
+    height: 100px;
+    font-size: 2em;
+" id="encoded-polyline" ></textarea>
     </div>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBMtYmJp8Hhtxn-6TVi0U4y_Mh_HW6tCS0&libraries=geometry,places&callback=initMap"
         async defer></script>
@@ -245,5 +189,14 @@ function gcode(pid) {
       });
     </script>
     
-  </body>
+    
+
+            </div>
+		</div>
+	</div>
+</div>
+	<?php include '../footer.php'; ?>
+
+	
+</body>
 </html>
